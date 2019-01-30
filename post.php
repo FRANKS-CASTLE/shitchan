cc6329956d7cc9ef3158b43177a5274168dddbac
Musca <musturtwig@gmail.com>
2019-01-31 00:02:36 +0800

    test

--- post.php
+++ post.php
@@ -5,7 +5,7 @@
 require "./inc/functions.php";
 require "./inc/anti-bot.php";
 
-mysql_query("set names 'utf8'");
+mysql_query("set names 'gbk'");
 // Fix for magic quotes
 if (get_magic_quotes_gpc()) {
 	function strip_array($var) {
